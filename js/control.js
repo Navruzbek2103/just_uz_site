48311360eb3566c10f290806aebff084d83a1ab2
Navruzbek2103 <navrozbek2306@gmail.com>
2025-11-18 15:35:21 +0500

    Fixed select data in intro section

--- js/control.js
+++ js/control.js
@@ -7,7 +7,9 @@ const elBody = document.querySelector("body")
 let counterClickDirection = 0;
 function showSelectDirectionDropdownFunc(){
     elBody.addEventListener("click", (p) => {
-        
+        if(p.target.className === "intro-wrapper-directionItem" || p.target.className === "intro-wrapper-directionItem-link"){
+            getSelectDirection.childNodes[1].textContent = p.target.textContent;
+        }
         
         if(p.target.className === "intro-wrapper-item-textDirection" || p.target.className === "intro-wrapper-item intro-wrapper-itemDirection" || p.target.className === "intro-wrapper-item-downArrowRole" || p.target.className === "intro-wrapper-item-downArrowRole intro-wrapper-item-downArrowRole-active"){
             counterClickDirection++;
@@ -15,7 +17,8 @@ function showSelectDirectionDropdownFunc(){
             getSelectAreaList.previousElementSibling.classList.remove("intro-wrapper-item-downArrowArea-active")
             if(counterClickDirection % 2 !== 0){
                 getSelectDirectionList.classList.add("intro-wrapper-directionList-active")
-                getSelectDirectionList.previousElementSibling.classList.add("intro-wrapper-item-downArrowRole-active")                
+                getSelectDirectionList.previousElementSibling.classList.add("intro-wrapper-item-downArrowRole-active")    
+                            
             }
             
             else if(counterClickDirection % 2 === 0){
@@ -33,7 +36,9 @@ showSelectDirectionDropdownFunc()
 
 function showSelectAreaDropdownFunc(){
     elBody.addEventListener("click", (e) => {
-``        
+        if(e.target.className === "intro-wrapper-selectAreaItem" || e.target.className === "intro-wrapper-selectAreaItem-link"){
+            getSelectArea.childNodes[1].textContent = e.target.textContent;
+        }
         if(e.target.className === "intro-wrapper-item-textArea" || e.target.className === "intro-wrapper-item intro-wrapper-itemArea" || e.target.className === "intro-wrapper-item-downArrowArea" || e.target.className === "intro-wrapper-item-downArrowArea intro-wrapper-item-downArrowArea-active"){
             counterClickDirection++;
             getSelectDirectionList.classList.remove("intro-wrapper-directionList-active")
